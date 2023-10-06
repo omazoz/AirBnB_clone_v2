70a0e586290ce8e9cf77ead0085401b27c410342
omazoz <oumaymamazoz12@gmail.com>
2023-10-06 15:34:05 +0100

    update*

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-"""Author: Talaini
-  function do_pack that generates a .tgz archive"""
-
-
-from fabric.api import *
+"""Fabric script that generates a .tgz archive from the contents of the
+web_static folder of your AirBnB Clone repo, using the function do_pack."""
+from fabric.api import local
 from datetime import datetime
+import os
 
 
 def do_pack():
-    """ Fabric script that generates a .tgz archive"""
-    local("sudo mkdir -p versions")
-    gndate = datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = "versions/web_static_{}.tgz".format(gndate)
-    result = local("sudo tar -cvzf {} web_static".format(filename))
-    if result.succeeded:
-        return filename
-    else:
+    """Function to generate a .tgz archive from web_static folder."""
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "web_static_{}.tgz".format(date)
+    file_path = os.path.join("versions", file_name)
+    print("Packing web_static to {}".format(file_path))
+
+    try:
+        local("tar -czvf {} -C web_static .".format(file_path))
+        file_size = os.path.getsize(file_path)
+        print("web_static packed: {} -> {} Bytes".format(file_path, file_size)) 
+        return file_path
+    except Exception as e:
+        print("Error: {}".format(e))
         return None
