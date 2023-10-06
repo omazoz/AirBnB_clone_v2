a2bd776b835d8ed2486b07428fda44e8b80a8bbe
omazoz <oumaymamazoz12@gmail.com>
2023-10-06 15:16:25 +0100

    update*

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,30 +1,20 @@
 #!/usr/bin/env bash
-#Author: Talaini : sets up  web servers for the deployment
-
-# Install Nginx if it not already installed
-apt-get update
+# Script that sets up your web servers for the deployment of web_static
+apt-get -y update
 apt-get -y install nginx
-# Create the folder /data/ if it doesn’t already exist
 mkdir -p /data/web_static/releases/test/
-# Create the folder /data/web_static/shared/ if it doesn’t already exist
 mkdir -p /data/web_static/shared/
-# Create a fake HTML file /data/web_static/releases/test/index.html
-echo '<html>
+echo "<html>
   <head>
   </head>
   <body>
-    <h1>Alx Talaini</h1>
+    Hello World!
   </body>
-</html>' > /data/web_static/releases/test/index.html
-# Create a symbolic link /data/web_static/current linked to the
-# .../data/web_static/releases/test/ folder. If the symbolic link already
-# exists, it should be deleted and recreated every time the script is ran.
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-# Give ownership of the /data/ folder to the ubuntu user AND group This should be recursive;
-chown -hR ubuntu:ubuntu /data/
-# Update the Nginx configuration to serve the content of
-# /data/web_static/current/ to hbnb_static
-# (ex: https://mydomainname.tech/hbnb_static).
-sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
-#restart Nginx
+</html>" > /data/web_static/releases/test/index.html
+if [ -L "/data/web_static/current" ]; then
+	rm /data/web_static/current
+fi
+ln -s /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart
