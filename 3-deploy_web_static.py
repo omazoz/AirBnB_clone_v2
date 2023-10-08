40ff0ffe794c5871c5c51de5034f9b7e30f644e6
Oumayma Mazoz <91220282+omazoz@users.noreply.github.com>
2023-10-08 23:18:48 +0100

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,48 +3,50 @@
 Fabric script that distributes an archive to your web servers,
 using the function do_deploy.
 """
-from fabric.api import run, put, env
-from os import path
+from fabric.api import env, local, put, run
+from datetime import datetime
+from os.path import exists, isdir
 
 env.hosts = ['54.173.140.10', '3.85.1.16']
+def do_pack():
+    """ Fabric script that generates a .tgz archive"""
+    local("sudo mkdir -p versions")
+    gndate = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(gndate)
+    result = local("sudo tar -cvzf {} web_static".format(filename))
+    if result.succeeded:
+        return filename
+    else:
+        return None
+
 
 def do_deploy(archive_path):
-    """Function to distribute an archive to web servers."""
-    if not path.exists(archive_path):
+    """ deploy an archive to  servers
+    """
+    if exists(archive_path) is False:
         return False
-    
     try:
-        arch_name = path.basename(archive_path)
-        arch_no_ext = path.splitext(arch_name)[0]
-
-        # Upload the archive to /tmp/ on the remote server
+        fn_with_ext = os.path.basename(archive_path)
+        fn_no_ext, ext = os.path.splitext(fn_with_ext)
+        dpath = "/data/web_static/releases/"
         put(archive_path, "/tmp/")
-
-        # Create the release directory and extract the archive
-        run("mkdir -p /data/web_static/releases/{}/".format(arch_no_ext))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(arch_name, arch_no_ext))
-
-        # Remove the uploaded archive from /tmp/
-        run("rm /tmp/{}".format(arch_name))
-
-        # Move contents from web_static subdirectory to the release directory
-        run(("mv /data/web_static/releases/{}/web_static/* " +
-            "/data/web_static/releases/{}/").format(arch_no_ext, arch_no_ext))
-
-        # Remove the now-empty web_static subdirectory
-        run("rm -rf /data/web_static/releases/{}/web_static/"
-            .format(arch_no_ext))
-
-        # Update the symbolic link to point to the new release
-        run(("rm -rf /data/web_static/current && " +
-            "ln -s /data/web_static/releases/{}/ /data/web_static/current")
-            .format(arch_no_ext))
-
+        run("rm -rf {}{}/".format(dpath, fn_no_ext))
+        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
+        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
+        run("rm /tmp/{}".format(fn_with_ext))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
+        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
+        print("New version deployed!")
         return True
-    except FileNotFoundError:
-        print("Error: The archive file does not exist.")
+    except Exception:
         return False
-    except Exception as e:
-        print("Error: {}".format(e))
+
+
+def deploy():
+    """ do path an do deploy"""
+    archive_path = do_pack()
+    if archive_path is None:
         return False
+    return do_deploy(archive_path)
